050802814b8ec97e00c9677bb0f2a973a175f4a4
heio95 <davidkyvik@gmail.com>
2021-05-21 19:15:35 +0200

    Viewers

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -60,7 +60,7 @@ const Wrap = styled.div`
     height: 100%;
     box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
       rgb(0 0 0 / 73%) 0px 16px 10px -10px;
-    transition-duration: 280ms;
+    transition-duration: 250ms;
 
     &:hover {
       border: 4px solid rgba(249, 249, 249, 0.8);
--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -8,16 +8,16 @@ function Viewers() {
         <img src="/images/viewers-disney.png" />
       </Wrap>
       <Wrap>
-        <img src="/images/viewers-disney.png" />
+        <img src="/images/viewers-pixar.png" />
       </Wrap>
       <Wrap>
-        <img src="/images/viewers-disney.png" />
+        <img src="/images/viewers-marvel.png" />
       </Wrap>
       <Wrap>
-        <img src="/images/viewers-disney.png" />
+        <img src="/images/viewers-starwars.png" />
       </Wrap>
       <Wrap>
-        <img src="/images/viewers-disney.png" />
+        <img src="/images/viewers-national.png" />
       </Wrap>
     </Container>
   );
@@ -37,10 +37,18 @@ const Container = styled.div`
 const Wrap = styled.div`
   border-radius: 10px;
   border: 3px solid rgba(249, 249, 249, 0.1);
+  box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+    rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
 
   img {
     width: 100%;
     height: 100%;
     object-fit: cover;
+  }
+
+  &:hover {
+    transform: scale(1.05);
+    border-color: rgba(249, 249, 249, 0.8);
   }
 `;
